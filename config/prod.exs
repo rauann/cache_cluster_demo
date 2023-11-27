cd0a4519f528aeb2127e2c4fa8948768e3031ed8
rauan <rauann.assis@gmail.com>
2023-11-27 21:41:14 +0100

    Setup prod endpoint check_origin

--- config/prod.exs
+++ config/prod.exs
@@ -5,3 +5,5 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+
+config :cache_cluster_demo, CacheClusterDemoWeb.Endpoint, check_origin: ["//*.amazonaws.com"]
