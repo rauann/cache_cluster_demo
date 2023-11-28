3763cd8087fc653cb55c21e76950a2e37ff5010e
rauan <rauann.assis@gmail.com>
2023-11-28 07:55:42 +0100

    Setup prod libcluster

--- config/prod.exs
+++ config/prod.exs
@@ -7,3 +7,15 @@ config :logger, level: :info
 # of environment variables, is done on config/runtime.exs.
 
 config :cache_cluster_demo, CacheClusterDemoWeb.Endpoint, check_origin: ["//*.amazonaws.com"]
+
+config :libcluster,
+  topologies: [
+    ecs: [
+      strategy: Cluster.Strategy.DNSPoll,
+      config: [
+        polling_interval: 5_000,
+        query: "development-cache-cluster-demo-513612322.eu-north-1.elb.amazonaws.com",
+        node_basename: "cache_cluster_demo"
+      ]
+    ]
+  ]
